55e26d5cebbe76adf1999c21f56f34bb19e2fed5
Marwa <marwalbaadani@gmail.com>
2020-04-28 15:09:01 -0400

    working

--- partition.cpp
+++ partition.cpp
@@ -5,107 +5,112 @@
 
 using namespace std;
 
-int left(int i)
+struct MaxHeap
 {
-
-    return (2 * i) + 1;
-}
-
-int right(int i)
-{
-
-    return (2 * i) + 2;
-}
-
-int parent(int i)
-{ // i-1?
-    return (floor(i / 2));
-}
-
-void swap(vector<int> H, int index1, int index2)
-{
-
-    for (int i = 0; i < H.size(); i++)
-    {
-        std::cout << H.at(i) << ' ';
-    }
-    cout << endl;
-    int temp = H.at(index1);
-    H.at(index1) = H.at(index2);
-    H.at(index2) = temp;
-
-    for (int i = 0; i < H.size(); i++)
-    {
-        std::cout << H.at(i) << ' ';
-    }
-    cout << endl;
-}
-
-void insert(vector<int> H, int v)
-{
-    H.push_back(v);
-    int N = H.size();
-    while (H[parent(N)] < H[N] && H[0] != N)
-    {
-        swap(H, H[parent(N)], H[N]);
-        N = parent(N);
-    }
-}
-
-void maxHeapify(vector<int> H, int N)
-{
-    //    make_tuple(left(N), right(N));
-    int l = left(N);
-    int r = right(N);
-
-    int largest = -1;
-
-    if (l < H.size() && H[l] > H[N])
-    {
-        // cout << "left of " << H[N] << " is" << H[l] << endl;
-        // cout << "N = " << N << " and l =" << left(N) << endl;
-        largest = l;
-    }
-    else
+    vector<int> H;
+    int left(int i)
     {
 
-        largest = N;
+        return (2 * i) + 1;
     }
-    if (r < H.size() && H[r] > H[largest])
-    {
-        // cout << "right of " << H[N] << " is" << H[r] << endl;
-        largest = r;
-    }
-    if (largest != N)
+
+    int right(int i)
     {
 
-        swap(H, N, largest);
-        // cout << "swapped " << H[N] << " and " << H[largest] << endl;
-        maxHeapify(H, largest);
+        return (2 * i) + 2;
     }
-}
 
-void extractMax(vector<int> H)
-{
-    if (!H.empty())
+    int parent(int i)
     {
-        int max = H.at(0);
-        H[0] = H[H.size()];
-        H.pop_back(); //size(H) -= 1
-        maxHeapify(H, 0);
+        return (floor((i - 1) / 2));
     }
-}
 
-void buildHeap(vector<int> A)
-{
-
-    for (int i = floor(A.size() / 2) + 1; i >= 0; i--)
+    void swap(int index1, int index2)
     {
-        cout << "i " << i << endl;
-        maxHeapify(A, i);
-    }
-}
 
+        int temp = H[index1];
+        H[index1] = H[index2];
+        H[index2] = temp;
+
+        for (int i = 0; i < H.size(); i++)
+        {
+            std::cout << H.at(i) << ' ';
+        }
+        cout << endl;
+    }
+
+    void insert(int v)
+    {
+        H.push_back(v);
+        int N = H.size();
+        while (H[parent(N)] < H[N] && H[0] != N)
+        {
+            swap(H[parent(N)], H[N]);
+            N = parent(N);
+        }
+    }
+
+    void maxHeapify(int N)
+    {
+        //    make_tuple(left(N), right(N));
+        int l = left(N);
+        int r = right(N);
+
+        int largest = -1;
+
+        if (l < H.size() && H[l] > H[N])
+        {
+            // cout << "left of " << H[N] << " is" << H[l] << endl;
+            // cout << "N = " << N << " and l =" << left(N) << endl;
+            largest = l;
+        }
+        else
+        {
+
+            largest = N;
+        }
+        if (r < H.size() && H[r] > H[largest])
+        {
+            // cout << "right of " << H[N] << " is" << H[r] << endl;
+            largest = r;
+        }
+        if (largest != N)
+        {
+            cout << "swapping " << H[N] << " and " << H[largest] << endl;
+            swap(N, largest);
+            maxHeapify(largest);
+        }
+    }
+    void push(int key)
+    {
+        // insert the new element to the end of the vector
+        H.push_back(key);
+
+        // get element index and call heapify-up procedure
+        int index = H.size() - 1;
+        maxHeapify(index);
+    }
+    void extractMax()
+    {
+        if (!H.empty())
+        {
+            int max = H.at(0);
+            H[0] = H[H.size()];
+            H.pop_back(); //size(H) -= 1
+            maxHeapify(0);
+        }
+    }
+
+    void buildHeap()
+    {
+
+        for (int i = floor(H.size() / 2) + 1; i >= 0; i--)
+        {
+            cout << "i = " << i << endl;
+            maxHeapify(i);
+        }
+    }
+};
 int main(int argc, char **argv)
 {
 
@@ -113,9 +118,21 @@ int main(int argc, char **argv)
     int algo = atoi(argv[2]);
     char *file = argv[3];
 
-    int arr[] = {10, 81, 3, 600, 4, 700, 9};
+    int arr[] = {2, 1, 4, 3, 6, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     vector<int> a(arr, arr + n);
+
+    MaxHeap pq;
+
+    // Note - Priority is decided by element's value
+
+    pq.push(2);
+    pq.push(1);
+    pq.push(4);
+
+    pq.push(3);
+    pq.push(6);
+    pq.push(5);
 
     // sort(vec.begin(), vec.end());
     // reverse(vec.begin(), vec.end());
@@ -136,16 +153,17 @@ int main(int argc, char **argv)
     {
         std::cout << a.at(i) << ' ';
     }
-
+    cout << endl;
     // now we make a max heap
-    buildHeap(a);
 
     cout << endl
          << "hoe dont do it : ";
 
-    for (int i = 0; i < a.size(); i++)
+    pq.buildHeap();
+
+    for (int i = 0; i < pq.H.size(); i++)
     {
-        std::cout << a.at(i) << ' ';
+        std::cout << pq.H.at(i) << ' ';
     }
     cout << endl;
 }
