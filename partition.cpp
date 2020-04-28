ccda1fdb4b032516b85fa5fb2f0a5c3201f2e7ff
Marwa <marwalbaadani@gmail.com>
2020-04-28 15:15:38 -0400

    cleaned up max heap

--- partition.cpp
+++ partition.cpp
@@ -8,6 +8,7 @@ using namespace std;
 struct MaxHeap
 {
     vector<int> H;
+
     int left(int i)
     {
 
@@ -27,7 +28,6 @@ struct MaxHeap
 
     void swap(int index1, int index2)
     {
-
         int temp = H[index1];
         H[index1] = H[index2];
         H[index2] = temp;
@@ -52,44 +52,31 @@ struct MaxHeap
 
     void maxHeapify(int N)
     {
-        //    make_tuple(left(N), right(N));
         int l = left(N);
         int r = right(N);
-
         int largest = -1;
 
         if (l < H.size() && H[l] > H[N])
-        {
-            // cout << "left of " << H[N] << " is" << H[l] << endl;
-            // cout << "N = " << N << " and l =" << left(N) << endl;
             largest = l;
-        }
         else
-        {
-
             largest = N;
-        }
         if (r < H.size() && H[r] > H[largest])
-        {
-            // cout << "right of " << H[N] << " is" << H[r] << endl;
             largest = r;
-        }
         if (largest != N)
         {
-            cout << "swapping " << H[N] << " and " << H[largest] << endl;
             swap(N, largest);
             maxHeapify(largest);
         }
     }
+
+    // remind me @josh
     void push(int key)
     {
-        // insert the new element to the end of the vector
         H.push_back(key);
-
-        // get element index and call heapify-up procedure
         int index = H.size() - 1;
         maxHeapify(index);
     }
+
     void extractMax()
     {
         if (!H.empty())
@@ -118,21 +105,14 @@ int main(int argc, char **argv)
     int algo = atoi(argv[2]);
     char *file = argv[3];
 
-    int arr[] = {2, 1, 4, 3, 6, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    vector<int> a(arr, arr + n);
+    MaxHeap A;
 
-    MaxHeap pq;
-
-    // Note - Priority is decided by element's value
-
-    pq.push(2);
-    pq.push(1);
-    pq.push(4);
-
-    pq.push(3);
-    pq.push(6);
-    pq.push(5);
+    A.push(2);
+    A.push(1);
+    A.push(4);
+    A.push(3);
+    A.push(6);
+    A.push(5);
 
     // sort(vec.begin(), vec.end());
     // reverse(vec.begin(), vec.end());
@@ -146,24 +126,12 @@ int main(int argc, char **argv)
 
     // inFile.close();
 
-    cout
-        << "The vector elements are : ";
+    A.buildHeap();
 
-    for (int i = 0; i < a.size(); i++)
-    {
-        std::cout << a.at(i) << ' ';
-    }
-    cout << endl;
-    // now we make a max heap
-
-    cout << endl
-         << "hoe dont do it : ";
-
-    pq.buildHeap();
-
-    for (int i = 0; i < pq.H.size(); i++)
-    {
-        std::cout << pq.H.at(i) << ' ';
-    }
-    cout << endl;
+    // for testing
+    // for (int i = 0; i < A.H.size(); i++)
+    // {
+    //     std::cout << A.H.at(i) << ' ';
+    // }
+    // cout << endl;
 }
